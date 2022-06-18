af9fa8cd8ed6a04032562b20e66a69122b92e752
anadunning <anakpedrosa@hotmail.com>
2022-06-18 18:34:37 -0300

    JS Fundamentals - Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,33 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
-console.log('Hello!');
 console.log(40+8+23-10);
+
+console.log('Ann!');
+console.log(23);
+
+let firstName = "Matilda";
+let first = "John";
+let firstNamePerson = "Bob";
+let first_name_person = "Clare";
+
+// Variable names cannot start with a number.
+// let 3year = 35; => This is wrong.
+
+// Variable names cannot contain characters, except $ (dolar sign).
+// let jonas&matilda = "JM"; => This is wrong.
+
+// Variable names cannot contain reserved keywords.
+// let new = 25; => This is wrong.
+
+let $function = 25; // This is allowed. Although there is the function keyword,
+                   // the dolar sign starts the variable name.
+
+console.log($function);
+console.log(first);
+console.log(firstNamePerson);
+console.log(first_name_person);
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
