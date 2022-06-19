aea1121c640bfa34b518ae19655ff7f1343db000
anadunning <anakpedrosa@hotmail.com>
2022-06-19 20:13:06 -0300

    JS Fundamentals - Practice Assignment 4

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,17 +1,29 @@
 // ASSIGNMENT:
-// 1. Declare a variable called 'isIsland' and set its value according to your
-// country. The variable should hold a Boolean value. Also declare a variable
-// 'language', but don't assign it any value yet.
-// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
-// to the console.
+// 1. If your country split in half, and each half would contain half the population,
+// then how many people would live in each half?
+// 2. Increase the population of your country by 1 and log the result to the console
+// 3. Finland has a population of 6 million. Does your country have more people than
+// Finland?
+// 4. The average population of a country is 33 million people. Does your country
+// have less people than the average country?
+// 5. Based on the variables you created, create a new variable 'description'
+// which contains a string with this format: 'Portugal is in Europe, and its 11 million
+// people speak portuguese'.
 
-let isIsland = true;
-let language;
-let country = "United Kingdom";
-let continent = "Europe";
-let population = "60,000,000";
+const country = "United Kingdom";
+const language = "English";
+const continent = "Europe";
+const isIsland = true;
 
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+let population = 60;
+console.log(population / 2);
+
+population++;
+console.log( population);
+
+console.log(population > 6);
+console.log(population < 33);
+
+let description = country + " is in " + continent + ", and its " + population + " million people speak " + language + ".";
+
+console.log(description);
