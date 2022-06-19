da1e72b7ef8b85155ce1dbfe853840c4bd66da49
anadunning <anakpedrosa@hotmail.com>
2022-06-19 15:54:53 -0300

    JS Fundamentals - Operator Precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,31 +1,16 @@
-// Math operators
+// Operator precedence
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
-
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
-
-const firstName = 'John';
-const lastName = 'Smith';
-console.log(firstName, lastName);
-console.log(firstName + lastName);
-console.log(firstName + " " + lastName);
-
-// Assignment operators
-let x = 10 + 5;
-x += 10;
-x *= 4;
-x++;
-x--;
-x--;
-console.log(x);
-
-// Comparison operators
-console.log(ageJonas > ageSarah);
-console.log(ageSarah >= 18);
-
-const isFullAge = ageSarah >= 18;
-console.log(isFullAge);
 
 console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas  + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+// Check the website below for reference:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
