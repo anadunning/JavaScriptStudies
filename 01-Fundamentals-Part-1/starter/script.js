9f5e559e08580509cef0336acb70afd3c043a65e
anadunning <anakpedrosa@hotmail.com>
2022-06-19 00:12:20 -0300

    JS Fundamentals - let, const and var

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let javaScriptIsFun = true;
+/* let javaScriptIsFun = true;
 console.log(javaScriptIsFun);
 
 console.log(typeof javaScriptIsFun);
@@ -17,3 +17,27 @@ console.log(typeof year);
 
 // This is a bug, because null will come up with as an object. However, it is a kind of undefined.
 console.log(typeof null);
+*/
+
+// let, const and var
+// We use let to declare variables that can be changed later.
+// See example below:
+let age = 30;
+// Reasssigning a new value to the variable
+age = 31;
+
+// const: we assign to variables which are not supposed to change at any point.
+// Example:
+const birthYear = 1991;
+// birthYear = 1990; // This will generate an Uncaught TypeError.
+
+// Also, it is not possible to declare an empty const variable, i.e. a const which was not initialized.
+// Example:
+// const job; // This will generate an Uncaught SyntaxError.
+
+// As BEST PRACTICE, use const by default
+// and only use let if you are sure that the variable will change in the future.
+
+// var should be completely avoided. It works in the same way as let.
+var job = 'programmer';
+job = 'teacher';
