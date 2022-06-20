dcd06764dc06048ec34079b7ae7a827142cc2a6e
anadunning <anakpedrosa@hotmail.com>
2022-06-19 22:02:09 -0300

    JS Fundamentals - Strings and Template Literals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,24 +1,19 @@
-// Coding Challenge #1
-// Mark and John are trying to compare their BMI (Body Mass Index), which is
-// calculated using the formula:
-// BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
-// Your tasks:
-// 1. Store Mark's and John's mass and height in variables
-// 2. Calculate both their BMIs using the formula (you can even implement both versions).
-// 3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
-// Test data:
-// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
-// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+// Strings and Template Literals
+const firstName = 'John';
+const job = 'engineer';
+const birthYear = 1990;
+const year = 2030;
 
-let massMark = 95;
-let heightMark = 1.88;
-let massJohn = 85;
-let heightJohn = 1.76;
+const john = "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + ".";
+console.log(john);
 
-let BMI_Mark = massMark / (heightMark ** 2);
-let BMI_John = massJohn / (heightJohn * heightJohn);
+// Using Template literal - using the tick ` and dolar sign $ and curly braces {}
+const johnNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}.`;
+console.log(johnNew);
 
-let markHigherBMI = BMI_Mark > BMI_John;
+console.log(`Just a regular string...`);
+console.log('String with \nmultiple \nlines.'); // With the usual quotation marks, we need to use \n to go to the next line.
 
-console.log(typeof markHigherBMI);
-console.log(BMI_Mark, BMI_John, markHigherBMI);
+console.log(`String with 
+multiple
+lines.`);   // With template literal, we can break the line and the string will accept that.
