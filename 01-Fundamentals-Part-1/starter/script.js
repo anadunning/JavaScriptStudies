617c531bce14d4301f9eb81ea0066c428b8c8281
anadunning <anakpedrosa@hotmail.com>
2022-06-20 12:08:31 -0300

    JS Fundamentals - Practice Assignment 6

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,9 @@
-// 1. Recreate the 'description' variable from the last assignment, this time
-// using the template literal syntax
+// 1. If your country's population is greater that 33 million, log a string like this to the
+// console: 'Portugal's population is above average'. Otherwise, log a string like
+// 'Portugal's population is 22 million below average' (the 22 is the average of 33
+// minus the country's population)
+// 2. After checking the result, change the population temporarily to 13 and then to
+// 130. See the different results, and set the population back to original
 
 const country = "United Kingdom";
 const language = "English";
@@ -7,6 +11,10 @@ const continent = "Europe";
 const isIsland = true;
 
 let population = 60;
-let description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`;
 
-console.log(description); 
+if (population > 33) {
+    console.log(`${country}'s population is above average.`);
+}
+else {
+    console.log(`${country}'s population is 22 million below average.`);
+}
