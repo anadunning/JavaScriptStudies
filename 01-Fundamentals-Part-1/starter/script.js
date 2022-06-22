f244329dbfaff38c6beb6eb08b1867d8801bea99
anadunning <anakpedrosa@hotmail.com>
2022-06-21 21:13:45 -0300

    JS Fundamentals - Practice Assignment 8

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,18 +1,23 @@
-// Logical Operators
-const hasDriversLicence = true;
-const hasGoodVision = true;
+// ASSIGNMENT:
+// 1. Comment out the previous code so the prompt doesn't get in the way
+// 2. Let's say Sarah is looking for a new country to live in. She wants to live in a
+// country that speaks english, has less than 50 million people and is not an island.
+// 3. Write an if statement to help Sarah figure out if your country is right for her.
+// You will need to write a condition that accounts for all of Sarah's criteria. Take
+// your time with this, and check part of the solution if necessary.
+// 4. If yours is the right country, log a string like this: 'You should live in Portugal :)'.
+// If not, log 'Portugal does not meet your criteria :('
+// 5. Probably your country does not meet all the criteria. So go back and temporarily
+// change some variables in order to make the condition true (unless you live in Canada :D)
 
-console.log(hasDriversLicence && hasGoodVision);
-console.log(hasDriversLicence || hasGoodVision);
-console.log(!hasDriversLicence);
+const country = 'Canada';
+const language = 'English';
+const population = 30;
+const isIsland = false;
 
-const shouldDrive = hasDriversLicence && hasGoodVision;
-const isTired = false;
-console.log(hasDriversLicence && hasGoodVision && isTired);
-
-if (shouldDrive && !isTired) {
-    console.log('Sarah is able to drive!');
+if (language === 'English' && population < 50 && !isIsland) {
+    console.log(`You should live in ${country}.`);
 }
 else {
-    console.log('Someone else should drive.');
+    console.log(`${country} does not meet your criteria.`);
 }
