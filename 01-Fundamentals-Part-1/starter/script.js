c08cc3c02b6aa10ff4b2b24674977464dd6c7f01
anadunning <anakpedrosa@hotmail.com>
2022-06-22 00:01:31 -0300

    JS Fundamentals - The Ternary Operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,45 +1,17 @@
-// The switch statement
-const day = 'Wednesday';
+// The Ternary Operator
+const age = 17;
+age >= 18 ? console.log('I like drinking wine.') : console.log('I like drinking water.');
 
-switch (day) {
-    case 'Monday':
-        console.log('Plan course structure.');
-        console.log('Go to coding meetup');
-        break;
-    case 'Tuesday':
-        console.log('Prepare theory videos.');
-        break;
-    case 'Wednesday':
-    case 'Thursday':
-        console.log('Write code examples.');
-        break;
-    case 'Friday':
-        console.log('Record videos');
-        break;
-    case 'Saturday':
-    case 'Sunday':
-        console.log('Enjoy the weekend!');
-        break;
-    default:
-        console.log('Not a valid day!');
-}
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
 
-if (day === 'Monday') {
-    console.log('Plan course structure.');
-    console.log('Go to coding meetup');
-}
-else if (day === 'Tuesday') {
-    console.log('Prepare theory videos.');
-}
-else if (day === 'Wednesday' || day === 'Thursday') {
-    console.log('Write code examples.');
-}
-else if (day === 'Friday') {
-    console.log('Record videos');
-}
-else if (day === 'Saturday' || day === 'Sunday') {
-    console.log('Enjoy the weekend!');
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
 }
 else {
-    console.log('Not a valid day!');
+    drink2 = 'water';
 }
+console.log(drink2);
+
+console.log(`I like drinking ${age >= 18 ? 'wine' : 'water'}.`);
