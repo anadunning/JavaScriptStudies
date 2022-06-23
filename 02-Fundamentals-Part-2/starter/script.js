f6c137bc5f699cdc7521afdde109125856160e77
anadunning <anakpedrosa@hotmail.com>
2022-06-22 21:32:04 -0300

    JS Fundamentals - Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) hasDriversLicence = true;
+if (hasDriversLicence) console.log('I can drive :D');
+
+// When strict mode is enabled, it can help the developers to
+// avoid introducing bugs in the code.
