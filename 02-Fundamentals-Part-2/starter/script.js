60a0c4d1cd4a65410f3252be6772e8d2db5bbf6c
anadunning <anakpedrosa@hotmail.com>
2022-06-27 22:47:39 -0300

    JS Fundamentals - Dot vs Bracket Notation

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -16,3 +16,30 @@ const jonas = {
     job: 'teacher',
     friends: ['Michael', 'Peter', 'Steven']
 };
+console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+// Using the bracket notation
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends');
+console.log(jonas[interestedIn]);
+
+if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+}
+else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job and friends');
+}
+
+// Adding more elements to the object
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtman';
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael."
+console.log(`${jonas['first' + nameKey]} has ${jonas.friends.length} friends, and his best friends is called ${jonas.friends[0]}.`);
