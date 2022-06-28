1d93618d9e4656aa5fca59d182c3726b4e3491c9
anadunning <anakpedrosa@hotmail.com>
2022-06-27 21:29:57 -0300

    JS Fundamentals - Basic Array Operations (Methods)

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,36 +1,32 @@
-// Introduction to Arrays
-const friend1 = "Michael";
-const friend2 = "Bob";
-const friend3 = "Peter";
+// Basic Array Operations (Methods)
 
 const friends = ['Michael', 'Bob', 'Peter'];
+
+// Add elements into the array
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
 console.log(friends);
 
-const year = new Array(1991, 1984, 2008, 2020);
-
-console.log(friends[0]);
-console.log(friends[2]);
-
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
-
-friends[2] = 'Jay';
+// Remove elements from the array
+friends.pop(); // Removes the last element
+const popped = friends.pop();
 console.log(friends);
 
-const firstName = 'Jonas';
-const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
-console.log(jonas);
-console.log(jonas.length);
+friends.shift(); // Removes the first element
+console.log(friends);
 
-// Exercise: calculate the ages from the years in the array for the position 0, 1 and the last position.
-const calcAge = function (birthYear) {
-    return 2037 - birthYear;
+console.log(friends.indexOf('Bob'));
+console.log(friends.indexOf('Steven'));
+
+friends.push(23);
+console.log(friends.includes('Bob'));
+console.log(friends.includes('Steven'));
+console.log(friends.includes('23')); // It doesn't do type coercion.
+console.log(friends.includes(23));
+
+if (friends.includes('Bob')) {
+    console.log('You have a friend called Bob.');
 }
-const years = [1990, 1967, 1975, 2002, 2010, 2018];
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[years.length - 1]);
-console.log(age1, age2, age3);
-
-const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
-console.log(ages);
