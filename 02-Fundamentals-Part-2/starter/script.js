f77633e4e3f0c939c6d2332a098462d3be46c516
anadunning <anakpedrosa@hotmail.com>
2022-07-01 22:08:56 -0300

    JS Fundamentals - Looping Arrays, Breaking and Continuing

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,47 @@
-// LOOPS:
+// Looping Arrays, Breaking and Continuing
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
 
-// For loop keeps running while condition is true
-for (let rep = 1; rep <= 10; rep++) {
-    console.log(`Lifting weights repetition ${rep}!`);
+const types = [];
+
+for (let i = 0; i < jonasArray.length; i++) {
+    // Reading from jonasArray
+    console.log(jonasArray[i], typeof jonasArray[i]);
+
+    // Filling types array
+    // types[i] = typeof jonasArray[i];
+    types.push(typeof jonasArray[i]);
+}
+
+console.log(types);
+
+// Calculating ages and adding in an array using a loop
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+
+console.log(ages);
+
+// continue and break
+console.log('---- ONLY STRINGS ----');
+for (let i = 0; i < jonasArray.length; i++) {
+    if (typeof jonasArray[i] !== 'string') continue;
+
+    console.log(jonasArray[i], typeof jonasArray[i]);
+}
+
+console.log('---- BREAK WITH NUMBERS ----');
+for (let i = 0; i < jonasArray.length; i++) {
+    if (typeof jonasArray[i] === 'number') break;
+
+    console.log(jonasArray[i], typeof jonasArray[i]);
 }
