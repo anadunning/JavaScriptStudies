8cb20fb2bc4e749a854a4b0f147f782f174eaaa9
anadunning <anakpedrosa@hotmail.com>
2022-07-06 22:57:15 -0300

    JS Fundamentals - Practice Assignment 10 - Ternary Operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,30 +1,14 @@
-// 1. Use a switch statement to log the following string for the given 'language':
-// chinese or mandarin: 'MOST number of native speakers!'
-// spanish: '2nd place in number of native speakers'
-// english: '3rd place'
-// hindi: 'Number 4'
-// arabic: '5th most spoken language'
-// for all other simply log 'Great language too :D'
+// ASSIGNMENT: Ternary Operator
+// 1. If your country's population is greater than 33 million, use the ternary operator
+// to log a string like this to the console: 'Portugal's population is above average'.
+// Otherwise, simply log 'Portugal's population is below average'. Notice how only
+// one word changes between these two sentences!
+// 2. After checking the result, change the population temporarily to 13 and then to
+// 130. See the different results, and set the population back to original.
 
-const language = "English";
+const country = "UK";
+const population = 60000000;
 
-switch (language) {
-  case "Chinese":
-  case "Mandarin":
-    console.log("MOST number of native speakers.");
-    break;
-  case "Spanish":
-    console.log("2nd place in number of native speakers.");
-    break;
-  case "English":
-    console.log("3rd place.");
-    break;
-  case "Hindi":
-    console.log("Number 4.");
-    break;
-  case "Arabic":
-    console.log("5th most spoken language.");
-    break;
-  default:
-    console.log("Great language too.");
-}
+population > 33000000
+  ? console.log(`${country}'s population is above average.`)
+  : console.log(`${country}'s population is below average.`);
