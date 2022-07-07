fa6a9dfe7da2e810c456cd0cd376f1278772b163
anadunning <anakpedrosa@hotmail.com>
2022-07-06 22:37:33 -0300

    Practice Assignment 9 - Switch

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,19 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+console.log(temperatures[2]);
+
+const calcTempAmplitude = function () {
+  let max = -100000;
+
+  for (let i = 0; i < temperatures.length; i++) {
+    console.log(temperatures);
+  }
+};
+
+// if (temperatures[i] > max) {
+//   max = temperatures[i];
+//   return max;
+// }
