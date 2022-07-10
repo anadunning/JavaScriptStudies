fabef9beb23343c8d5fb72140d7ad2c320904c28
anadunning <anakpedrosa@hotmail.com>
2022-07-09 23:50:29 -0300

    JS Fundamentals - Practice Assignment 13 - Arrow Functions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,39 +1,24 @@
-// ASSIGNMENT: Function Declarations vs. Expressions
-// The world population is 7900 million people. Create a function declaration
-// called 'percentageOfWorld1' which receives a 'population' value, and
-// returns the percentage of the world population that the given population
-// represents. For example, China has 1441 million people, so it's about 18.2% of
-// the world population.
-// 2. To calculate the percentage, divide the given 'population' value by 7900
-// and then multiply by 100.
-// 3. Call 'percentageOfWorld1' for 3 populations of countries of your choice,
-// store the results into variables, and log them to the console.
-// 4. Create a function expression which does the exact same thing, called
-// 'percentageOfWorld2', and also call it with 3 country populations (can be
-// the same populations).
+// ASSIGNMENT: Arrow Function
+// 1. Recreate the last assignment, but this time create an arrow function called
+// 'percentageOfWorld3'
 
-// Function declaration
-function percentageOfWorld1(population) {
+// Arrow Function
+const percentageOfWorld3 = (population) => {
   return (population / 7900) * 100;
-}
+};
 
 const IndiaPopulation = 1380;
 const ChinaPopulation = 1441;
 const USAPopulation = 329;
 
 console.log(
-  percentageOfWorld1(IndiaPopulation),
-  percentageOfWorld1(ChinaPopulation),
-  percentageOfWorld1(USAPopulation)
+  percentageOfWorld3(IndiaPopulation),
+  percentageOfWorld3(ChinaPopulation),
+  percentageOfWorld3(USAPopulation)
 );
 
-// Function expression
-const percentageOfWorld2 = function (population) {
-  return (population / 7900) * 100;
-};
-
-const percBrazil = percentageOfWorld2(212.6);
-const percCanada = percentageOfWorld2(38.01);
-const percUK = percentageOfWorld2(67.22);
+const percBrazil = percentageOfWorld3(212.6);
+const percCanada = percentageOfWorld3(38.01);
+const percUK = percentageOfWorld3(67.22);
 
 console.log(percBrazil, percCanada, percUK);
