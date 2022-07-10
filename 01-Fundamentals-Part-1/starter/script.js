c46c568df7064e5f14967b9bca795f42dcc353d0
anadunning <anakpedrosa@hotmail.com>
2022-07-09 23:28:33 -0300

    JS Fundamentals - Practice Assignment 12 - Function Declarations vs Expressions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,27 +1,39 @@
-// ASSIGNMENT: Functions
-// 1. Write a function called 'describeCountry' which takes three parameters:
-// 'country', 'population' and 'capitalCity'. Based on this input, the
-// function returns a string with this format: 'Finland has 6 million people and its
-// capital city is Helsinki'
-// 2. Call this function 3 times, with input data for 3 different countries. Store the
-// returned values in 3 different variables, and log them to the console.
+// ASSIGNMENT: Function Declarations vs. Expressions
+// The world population is 7900 million people. Create a function declaration
+// called 'percentageOfWorld1' which receives a 'population' value, and
+// returns the percentage of the world population that the given population
+// represents. For example, China has 1441 million people, so it's about 18.2% of
+// the world population.
+// 2. To calculate the percentage, divide the given 'population' value by 7900
+// and then multiply by 100.
+// 3. Call 'percentageOfWorld1' for 3 populations of countries of your choice,
+// store the results into variables, and log them to the console.
+// 4. Create a function expression which does the exact same thing, called
+// 'percentageOfWorld2', and also call it with 3 country populations (can be
+// the same populations).
 
-const country = "UK";
-const population = 60;
-const capitalCity = "London";
-
-function describeCountry(country, population, capitalCity) {
-  return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
+// Function declaration
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
 }
 
-console.log(describeCountry(country, population, capitalCity));
-console.log(describeCountry("France", 55, "Paris"));
-console.log(describeCountry("USA", 220, "Washington"));
-console.log(describeCountry("Brazil", 190, "Brasilia"));
-console.log(describeCountry("Portugal", 50, "Lisbon"));
+const IndiaPopulation = 1380;
+const ChinaPopulation = 1441;
+const USAPopulation = 329;
 
-// Jonas' Answer:
-const descPortugal = describeCountry("Portugal", 10, "Lisbon");
-const descGermany = describeCountry("Germany", 83, "Berlin");
-const descFinland = describeCountry("Finland", 6, "Helsinki");
-console.log(descPortugal, descGermany, descFinland);
+console.log(
+  percentageOfWorld1(IndiaPopulation),
+  percentageOfWorld1(ChinaPopulation),
+  percentageOfWorld1(USAPopulation)
+);
+
+// Function expression
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+
+const percBrazil = percentageOfWorld2(212.6);
+const percCanada = percentageOfWorld2(38.01);
+const percUK = percentageOfWorld2(67.22);
+
+console.log(percBrazil, percCanada, percUK);
