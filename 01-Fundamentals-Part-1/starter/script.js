54437e3d5f4045ac150a16f49553374dd067c976
anadunning <anakpedrosa@hotmail.com>
2022-07-10 00:32:18 -0300

    JS Fundamentals - Practice Assignment 14 - Functions Calling other Functions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,24 +1,32 @@
-// ASSIGNMENT: Arrow Function
-// 1. Recreate the last assignment, but this time create an arrow function called
-// 'percentageOfWorld3'
+// ASSIGNMENT: Functions Calling Other Functions
+// 1. Create a function called 'describePopulation'. Use the function type you
+// like the most. This function takes in two arguments: 'country' and
+// 'population', and returns a string like this: 'China has 1441 million people,
+// which is about 18.2% of the world.'
+// 2. To calculate the percentage, 'describePopulation' call the
+// 'percentageOfWorld1' you created earlier
+// 3. Call 'describePopulation' with data for 3 countries of your choice.
 
-// Arrow Function
-const percentageOfWorld3 = (population) => {
+function percentageOfWorld1(population) {
   return (population / 7900) * 100;
+}
+
+const describePopulation = (country, population) => {
+  return `${country} has ${population} million people which is about ${percentageOfWorld1(
+    population
+  )} % of the world.`;
 };
 
-const IndiaPopulation = 1380;
-const ChinaPopulation = 1441;
-const USAPopulation = 329;
+const describeIndia = describePopulation("India", 212.6);
+const describeChina = describePopulation("China", 1441);
+const describeUSA = describePopulation("The USA", 329);
+const describeBrazil = describePopulation("Brazil", 212.6);
+const describeCanada = describePopulation("Canada", 38.01);
+const describeUK = describePopulation("The UK", 67.22);
 
-console.log(
-  percentageOfWorld3(IndiaPopulation),
-  percentageOfWorld3(ChinaPopulation),
-  percentageOfWorld3(USAPopulation)
-);
-
-const percBrazil = percentageOfWorld3(212.6);
-const percCanada = percentageOfWorld3(38.01);
-const percUK = percentageOfWorld3(67.22);
-
-console.log(percBrazil, percCanada, percUK);
+console.log(describeIndia);
+console.log(describeChina);
+console.log(describeUSA);
+console.log(describeBrazil);
+console.log(describeCanada);
+console.log(describeUK);
