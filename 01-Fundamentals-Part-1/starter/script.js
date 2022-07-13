a285b8f08da78a1f19efd676ba3b1b52b5056d8b
anadunning <anakpedrosa@hotmail.com>
2022-07-12 22:36:24 -0300

    JS Fundamentals - Practice Assignment 16 - Basic Array Operations (Methods)

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,25 +1,47 @@
-// ASSIGNMENT: Introduction to Arrays
-// 1. Create an array containing 4 population values of 4 countries of your choice.
-// You may use the values you have been using previously. Store this array into a
-// variable called 'populations'.
-// 2. Log to the console whether the array has 4 elements or not (true or false).
-// 3. Create an array called 'percentages' containing the percentages of the
-// world population for these 4 population values. Use the function
-// 'percentageOfWorld1' that you created earlier to compute the 4
-// percentage values.
+// ASSIGNMENT: Basic Array Operations (Methods)
+// Create an array containing all the neighbouring countries of a country of your
+// choice. Choose a country which has at least 2 or 3 neighbours. Store the array
+// into a variable called 'neighbours'
+// 2. At some point, a new country called 'Utopia' is created in the neighbourhood of
+// your selected country. So add it to the end of the 'neighbours' array
+// 3. Unfortunately, after some time, the new country is dissolved. So remove it from
+// the end of the array
+// 4. If the 'neighbours' array does not include the country ‘Germany’, log to the
+// console: 'Probably not a central European country :D'
+// 5. Change the name of one of your neighbouring countries. To do that, find the
+// index of the country in the 'neighbours' array, and then use that index to
+// change the array at that index position. For example, you can search for
+// 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
 
-const populations = [83.24, 45.38, 144.1, 220.9];
-const percentages = [];
+const neighbours = [
+  "Argentina",
+  "Paraguai",
+  "Uruguai",
+  "Peru",
+  "Bolivia",
+  "Colombia",
+  "Venezuela",
+  "Guiana Inglesa",
+  "Suriname",
+  "Guiana Francesa",
+];
 
-console.log(populations);
-console.log(populations.length === 4);
+neighbours.push("Utopia");
+console.log(neighbours);
 
-function percentageOfWorld1(populations) {
-  return (populations / 7900) * 100;
+neighbours.pop();
+console.log(neighbours);
+
+if (!neighbours.includes("Germany")) {
+  console.log("Probably not a central European country :D");
 }
 
-for (let i = 0; i < populations.length; i++) {
-  percentages.push(percentageOfWorld1(populations[i]));
-}
+let index = neighbours.indexOf("Suriname");
+console.log(index);
 
-console.log(percentages);
+neighbours[8] = "Guiana Holandesa";
+console.log(neighbours);
+
+// Jonas's solution
+neighbours[neighbours.indexOf("Argentina")] = "Republica da Argentina";
+console.log(neighbours);
