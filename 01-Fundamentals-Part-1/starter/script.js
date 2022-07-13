3df852b869aa1ea481d0183c014977ab136112ac
anadunning <anakpedrosa@hotmail.com>
2022-07-12 22:46:27 -0300

    JS Fundamentals - Practice Assignment 17 - Introduction to Objects

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,47 +1,23 @@
-// ASSIGNMENT: Basic Array Operations (Methods)
-// Create an array containing all the neighbouring countries of a country of your
-// choice. Choose a country which has at least 2 or 3 neighbours. Store the array
-// into a variable called 'neighbours'
-// 2. At some point, a new country called 'Utopia' is created in the neighbourhood of
-// your selected country. So add it to the end of the 'neighbours' array
-// 3. Unfortunately, after some time, the new country is dissolved. So remove it from
-// the end of the array
-// 4. If the 'neighbours' array does not include the country ‘Germany’, log to the
-// console: 'Probably not a central European country :D'
-// 5. Change the name of one of your neighbouring countries. To do that, find the
-// index of the country in the 'neighbours' array, and then use that index to
-// change the array at that index position. For example, you can search for
-// 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+// ASSIGNMENT: Introduction to Objects
+// 1. Create an object called 'myCountry' for a country of your choice, containing
+// properties 'country', 'capital', 'language', 'population' and
+// 'neighbours' (an array like we used in previous assignments)
 
-const neighbours = [
-  "Argentina",
-  "Paraguai",
-  "Uruguai",
-  "Peru",
-  "Bolivia",
-  "Colombia",
-  "Venezuela",
-  "Guiana Inglesa",
-  "Suriname",
-  "Guiana Francesa",
-];
-
-neighbours.push("Utopia");
-console.log(neighbours);
-
-neighbours.pop();
-console.log(neighbours);
-
-if (!neighbours.includes("Germany")) {
-  console.log("Probably not a central European country :D");
-}
-
-let index = neighbours.indexOf("Suriname");
-console.log(index);
-
-neighbours[8] = "Guiana Holandesa";
-console.log(neighbours);
-
-// Jonas's solution
-neighbours[neighbours.indexOf("Argentina")] = "Republica da Argentina";
-console.log(neighbours);
+const myCountry = {
+  country: "Brazil",
+  capital: "Brasilia",
+  language: "Portuguese",
+  population: 212.6,
+  neighbours: [
+    "Argentina",
+    "Paraguai",
+    "Uruguai",
+    "Peru",
+    "Bolivia",
+    "Colombia",
+    "Venezuela",
+    "Guiana Inglesa",
+    "Suriname",
+    "Guiana Francesa",
+  ],
+};
