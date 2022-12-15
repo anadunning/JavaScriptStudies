ac25b7dd948990ca22eeb68838932834bfbe5a48
anadunning <anakpedrosa@hotmail.com>
2022-12-15 00:11:34 -0300

    Primitives vs Objects in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,159 +1,50 @@
 'use strict';
 
-// function calcAge(birthYear) {
-//   const age = 2037 - birthYear;
+let age = 30;
+let oldAge = age;
+age = 31;
 
-//   function printAge() {
-//     let output = `${firstName}, you are ${age}, born in ${birthYear}.`;
-//     console.log(output);
+console.log(age);
+console.log(oldAge);
 
-//     if (birthYear >= 1981 && birthYear <= 1996) {
-//       var millenial = true;
-//       // Creating NEW variable with same name as outer scope's variable
-//       const firstName = 'Ana';
-
-//       // Reassigning outer scope's variable
-//       output = 'NEW OUTPUT!';
-
-//       const str = `Oh, and you're a millenial, ${firstName}`;
-//       console.log(str);
-
-//       function add(a, b) {
-//         return a + b;
-//       }
-//     }
-//     // console.log(str);
-//     console.log(millenial);
-//     // console.log(add(2, 3));
-//     console.log(output);
-//   }
-//   printAge();
-
-//   return age;
-// }
-
-// const firstName = 'Jonas';
-// calcAge(1991);
-
-// // console.log(a);
-
-// // Hoisting - Variables
-// console.log(me);
-// // console.log(job);
-// // console.log(year);
-
-// var me = 'Ana';
-// let job = 'teacher';
-// const year = 1991;
-
-// // Hoisting - Functions
-
-// // console.log(addDec(2, 3));
-// // console.log(addExpr(2, 4));
-// // console.log(addArrow(2, 5));
-
-// function addDec(a, b) {
-//   return a + b;
-// }
-
-// const addExpr = function (a, b) {
-//   return a + b;
-// };
-
-// const addArrow = (a, b) => a + b;
-
-// // Example
-// console.log(undefined); // undefined is falsy
-// if (!numProducts) deleteShoppingCart(); // The conditional expression works because hoisting gets the
-// // var numProducts inside the if before reaching the var variable
-// // due to hoisting, it gets var variables and will show them
-// // as undefined which is a falsy value.
-// // Avoid using var to declare your variables, also declare your variables always before the scope.
-
-// var numProducts = 10;
-
-// function deleteShoppingCart() {
-//   console.log('All products deleted!');
-// }
-
-// var x = 1;
-// let y = 2;
-// const z = 3;
-
-// // Writing window in the console show all the properties for the window object.
-
-// console.log(x === window.x);
-// console.log(y === window.y);
-// console.log(z === window.z);
-
-// // The This keyword in practice
-// console.log(this);
-
-// const calculateAge = function (birthYear) {
-//   console.log(2037 - birthYear);
-//   console.log(this); // Here it'll come up as undefined
-// };
-// calculateAge(1991);
-
-// const calculateAgeArrow = birthYear => {
-//   console.log(2037 - birthYear);
-//   console.log(this); // Here this is coming up as window
-// };
-// calculateAgeArrow(1980);
-
-// const jonas = {
-//   year: 1991,
-//   calcAge: function () {
-//     console.log(this);
-//     console.log(2037 - this.year);
-//   },
-// };
-
-// jonas.calcAge(); // Here it comes up with the jonas object
-
-// const maria = {
-//   year: 2017,
-// };
-
-// maria.calcAge = jonas.calcAge;
-// maria.calcAge(); // Proves that the this keyword always point to the object which is calling the method.
-
-// var firstName = 'Matilda';
-
-const jonas = {
-  firstName: 'Jonas',
-  year: 1991,
-  calcAge: function () {
-    // console.log(this);
-    console.log(2037 - this.year);
-
-    // Solution 1: to preserve this and not let it come as undefined
-    // const self = this; // self or that
-    // const isMillenial = function () {
-    //   console.log(self);
-    //   console.log(self.year >= 1981 && self.year <= 1996);
-    //   // console.log(this.year >= 1981 && this.year <= 1996);
-    // };
-
-    // Solution 2
-    const isMillenial = () => {
-      console.log(this);
-      console.log(this.year >= 1981 && this.year <= 1996);
-      // console.log(this.year >= 1981 && this.year <= 1996);
-    };
-    isMillenial();
-  },
-
-  // greet: () => {
-  //   console.log(this);
-  //   console.log(`Hey ${this.firstName}`);
-  // },
-
-  greet: function () {
-    console.log(this);
-    console.log(`Hey ${this.firstName}`);
-  },
+const me = {
+  name: 'Jonas',
+  age: 30,
 };
 
-jonas.greet();
-jonas.calcAge();
+const friend = me;
+friend.age = 27;
+console.log('Friend:', friend);
+console.log('Me', me);
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage: ', jessica);
+console.log('After marriage: ', marriedJessica);
+
+// marriedJessica = {};
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+console.log('Before marriage: ', jessica2);
+console.log('After marriage: ', jessicaCopy);
